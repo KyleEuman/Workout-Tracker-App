27b2ed682b8f0998956d30de8911550cf97c31d1
Kyle Euman <euman_kyle@yahoo.com>
2021-05-13 15:05:59 -0400

    got server started

---
+++ Develop/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongojs = require('mongojs');
+
+const app =express();
+
+const databaseUrl ='Workout Tracker';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+ console.log('App running on port 3000!')
+})
+
+
